4a9de2faac7f8131cdfb4631d522b59c2d8ac8ca
Garrett Smith <garrettarhsmith@gmail.com>
2017-12-12 01:24:34 -0500

    Revert "Revert "v1.1""
    This reverts commit 54a43e831c091ef3ce26ef5c7db87f62b25aeb9e.

--- js/gen.js
+++ js/gen.js
@@ -1,3 +1,4 @@
+var on = false;
 
 var text = Array();
 
@@ -8,25 +9,22 @@ var currentLetter;
 var pastLetters = "";
 
 function gen() {
-  text = [];
-  var random;
+    if (on == false) {
+        on = true;
+        text = [];
+        var random;
 
-  for (var i = 0; i < 10; i++) {
-    random = words[Math.floor(Math.random() * 112)];
-    text.push(random);
-  }
-  currentWord = text[wordPos];
-  currentLetter = currentWord.charAt(letterPos);
-  document.getElementById('0').innerHTML = text[0];
-  document.getElementById('1').innerHTML = text[1];
-  document.getElementById('2').innerHTML = text[2];
-  document.getElementById('3').innerHTML = text[3];
-  document.getElementById('4').innerHTML = text[4];
-  document.getElementById('5').innerHTML = text[5];
-  document.getElementById('6').innerHTML = text[6];
-  document.getElementById('7').innerHTML = text[7];
-  document.getElementById('8').innerHTML = text[8];
-  document.getElementById('9').innerHTML = text[9];
+        for (var i = 0; i < 10; i++) {
+            random = words[Math.floor(Math.random() * 112)];
+            text.push(random);
+        }
+        currentWord = text[wordPos];
+        currentLetter = currentWord.charAt(letterPos);
+        for (var i = 0; i < 10; i++) {
+            document.getElementById(i).innerHTML = text[i];
+        }
 
-  $("#" + String(wordPos)).css("background-color", "lightblue");
+
+        changeColor("lightblue", wordPos);
+    }
 }
--- js/keyboard.js
+++ js/keyboard.js
@@ -1,56 +1,75 @@
-
 var keycode;
 var key;
 var input = document.getElementById('input');
 
 
 function changeColor(color, pos) {
-  $("#" + pos).css("background-color", color);
+    $("#" + pos).css("background-color", color);
 }
 
 
-function practice() {
-  var letters = currentWord.substring(0, input.value.length);
+function keyDown() {
+        if (input.value.length > 0 && keycode == 32) {
+          if (input.value == currentWord) {
+            $("#" + wordPos).css("color", "green");
+          }else {
+            $("#" + wordPos).css("color", "red");
+          }
+          $("#input").val("");
+          wordPos++;
+          if (wordPos >= 10) {
+            for (var i = 0; i < 10; i++) {
+              $("#" + i).css("color", "");
+            }
+              wordPos = 0;
+              changeColor("", 9);
+              on = false
+              gen();
+          }
+          currentWord = text[wordPos];
+          changeColor("", wordPos - 1);
+          changeColor("lightblue", wordPos);
+        }
+}
 
-  if (input.value == letters) {
-    changeColor("lightblue", wordPos);
-    if (input.value == currentWord) {
-      $("#input").val("");
-      wordPos++;
-      changeColor("", wordPos - 1);
-      if (wordPos >= 10) {
-        wordPos = 0;
-        changeColor("", 9);
-        gen();
-      }
-      currentWord = text[wordPos];
-      changeColor("lightblue", wordPos);
+function keyUp() {
+  if (on) {
+    var letters = currentWord.substring(0, input.value.length);
+
+    if (letterPos == 0 && input.value == " ") {
+        $("#input").val("");
+        changeColor("lightblue", wordPos);
     }
-  }else if (input.value != letters) {
-    changeColor("pink", wordPos);
+
+      if (input.value == letters) {
+          changeColor("lightblue", wordPos);
+      } else if (input.value != letters && input.value != " " && input.value != "") {
+          changeColor("pink", wordPos);
+      }
   }
 }
 
 
 
 document.addEventListener("keydown", function(e) {
-  keycode = e.keyCode;
-  key = "." + keycode;
-  $(key).css({
-    "background-color": "#6d97db",
-    "position": "relative",
-    "top": "2px", "left": "2px",
-    "box-shadow": "1px 1px 0px 0px rgba(150,150,150,0)"
-  })
-  practice();
+    keycode = e.keyCode;
+    key = "." + keycode;
+    $(key).css({
+        "background-color": "#6d97db",
+        "position": "relative",
+        "top": "2px",
+        "left": "2px",
+        "box-shadow": "1px 1px 0px 0px rgba(150,150,150,0)"
+    })
+    keyDown();
 });
 document.addEventListener("keyup", function(e) {
-  keycode = e.keyCode;
-  key = "." + keycode;
-  $(key).css({
-    "background-color": "#ddd",
-    "position": "static",
-    "box-shadow": "1px 1px 0px 0px rgba(150,150,150,1)"
-  })
-  practice();
+    keycode = e.keyCode;
+    key = "." + keycode;
+    $(key).css({
+        "background-color": "#e0e0e0",
+        "position": "static",
+        "box-shadow": "1px 1px 0px 0px rgba(150,150,150,1)"
+    })
+    keyUp();
 });
