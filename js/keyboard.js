54a43e831c091ef3ce26ef5c7db87f62b25aeb9e
Garrett Smith <garrettarhsmith@gmail.com>
2017-12-12 01:23:56 -0500

    Revert "v1.1"
    This reverts commit 993d6058d0918ac3113688e8c5ee9bae399b8c8c.

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,75 +1,56 @@
+
 var keycode;
 var key;
 var input = document.getElementById('input');
 
 
 function changeColor(color, pos) {
-    $("#" + pos).css("background-color", color);
+  $("#" + pos).css("background-color", color);
 }
 
 
-function keyDown() {
-        if (input.value.length > 0 && keycode == 32) {
-          if (input.value == currentWord) {
-            $("#" + wordPos).css("color", "green");
-          }else {
-            $("#" + wordPos).css("color", "red");
-          }
-          $("#input").val("");
-          wordPos++;
-          if (wordPos >= 10) {
-            for (var i = 0; i < 10; i++) {
-              $("#" + i).css("color", "");
-            }
-              wordPos = 0;
-              changeColor("", 9);
-              on = false
-              gen();
-          }
-          currentWord = text[wordPos];
-          changeColor("", wordPos - 1);
-          changeColor("lightblue", wordPos);
-        }
-}
+function practice() {
+  var letters = currentWord.substring(0, input.value.length);
 
-function keyUp() {
-  if (on) {
-    var letters = currentWord.substring(0, input.value.length);
-
-    if (letterPos == 0 && input.value == " ") {
-        $("#input").val("");
-        changeColor("lightblue", wordPos);
-    }
-
-      if (input.value == letters) {
-          changeColor("lightblue", wordPos);
-      } else if (input.value != letters && input.value != " " && input.value != "") {
-          changeColor("pink", wordPos);
+  if (input.value == letters) {
+    changeColor("lightblue", wordPos);
+    if (input.value == currentWord) {
+      $("#input").val("");
+      wordPos++;
+      changeColor("", wordPos - 1);
+      if (wordPos >= 10) {
+        wordPos = 0;
+        changeColor("", 9);
+        gen();
       }
+      currentWord = text[wordPos];
+      changeColor("lightblue", wordPos);
+    }
+  }else if (input.value != letters) {
+    changeColor("pink", wordPos);
   }
 }
 
 
 
 document.addEventListener("keydown", function(e) {
-    keycode = e.keyCode;
-    key = "." + keycode;
-    $(key).css({
-        "background-color": "#6d97db",
-        "position": "relative",
-        "top": "2px",
-        "left": "2px",
-        "box-shadow": "1px 1px 0px 0px rgba(150,150,150,0)"
-    })
-    keyDown();
+  keycode = e.keyCode;
+  key = "." + keycode;
+  $(key).css({
+    "background-color": "#6d97db",
+    "position": "relative",
+    "top": "2px", "left": "2px",
+    "box-shadow": "1px 1px 0px 0px rgba(150,150,150,0)"
+  })
+  practice();
 });
 document.addEventListener("keyup", function(e) {
-    keycode = e.keyCode;
-    key = "." + keycode;
-    $(key).css({
-        "background-color": "#e0e0e0",
-        "position": "static",
-        "box-shadow": "1px 1px 0px 0px rgba(150,150,150,1)"
-    })
-    keyUp();
+  keycode = e.keyCode;
+  key = "." + keycode;
+  $(key).css({
+    "background-color": "#ddd",
+    "position": "static",
+    "box-shadow": "1px 1px 0px 0px rgba(150,150,150,1)"
+  })
+  practice();
 });
