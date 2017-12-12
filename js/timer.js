cc2eb0f4100262140885f0a1dd20ef2f38d074eb
Garrett Smith <garrettarhsmith@gmail.com>
2017-12-12 14:11:27 -0500

    Beautify

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,11 @@
-
 var seconds = 60;
 
 setInterval(function() {
-  if (on && seconds > 0) {
-    seconds--;
-    document.getElementById('time').innerHTML = seconds + "s";
-  }else if (seconds <= 0) {
-    wpm = correct/5
-    document.getElementById('wpm').innerHTML = "WPM: " + wpm;
-  }
+    if (on && seconds > 0) {
+        seconds--;
+        document.getElementById('time').innerHTML = seconds + "s";
+    } else if (seconds <= 0) {
+        wpm = correct / 5;
+        document.getElementById('wpm').innerHTML = "WPM: " + wpm;
+    }
 }, 1000);
